ca476a3e065f993258e961588e2c546df2c1f33b
coxm <coxm@users.noreply.github.com>
2022-02-09 16:08:01 +0000

    Add option to disable model signals (#11)
    * Add disable_signals utility
    
    * Add LoggedCommand --disable-model-signals option
    
    Add a --disable-model-signals option to LoggedCommand that disables
    model-related signals during the execution of do_command.
    
    Notice this option does *not* disable signals propagated by the creation
    and saving of ManagementCommandLog operations, as this is unlikely to be
    the desired effect.
    
    * Amend comment

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,13 +1,19 @@
 import argparse
+import contextlib
 import datetime
+from unittest.mock import Mock, patch
 
 import pytest
 from django.core.management import call_command
+from django.db.models.signals import post_save
 
 from command_log.management.commands.base import isodate
 from command_log.models import ManagementCommandLog
+from command_log.utils.signals import MODEL_SIGNALS
 
 from .management.commands import test_command
+
+TEST_MODULE = "command_log.management.commands.base"
 
 
 @pytest.mark.django_db
@@ -47,6 +53,57 @@ def test_command__exit_code_2():
     assert log.exit_code == ManagementCommandLog.EXIT_CODE_PARTIAL
     assert log.output == str(test_command.DEFAULT_RETURN_VALUE)
     assert log.error == test_command.EXCEPTION_MSG
+
+
+@pytest.mark.django_db
+class TestDisableModelSignalsOption:
+    @pytest.mark.parametrize("model_signals_disabled", (True, False))
+    @patch(f"tests.management.commands.test_command.Command.do_command")
+    def test_disables_model_signals_during_do_command(
+        self,
+        do_command: Mock,
+        model_signals_disabled: bool,
+    ) -> None:
+        disabled_currently = ()
+        disabled_during_command = ()
+
+        def fake_do_command(*args, **kwargs):
+            nonlocal disabled_during_command, disabled_currently
+            disabled_during_command = disabled_currently
+
+        do_command.side_effect = fake_do_command
+
+        @contextlib.contextmanager
+        def fake_disable_signals(signals):
+            nonlocal disabled_currently
+            disabled_currently = signals
+            yield
+            disabled_currently = ()
+
+        command_args = ("--disable-model-signals",) if model_signals_disabled else ()
+
+        # Patch here so we don't need to mock __enter__ etc.
+        with patch(f"{TEST_MODULE}.disable_signals", fake_disable_signals):
+            call_command("test_command", *command_args)
+
+        assert disabled_currently == ()
+        assert disabled_during_command == (
+            MODEL_SIGNALS if model_signals_disabled else ()
+        )
+
+    def test_does_not_disable_management_command_log_signals(self):
+        received = {}
+
+        def receiver(sender, *args, **kwargs):
+            received["sender"] = sender
+            received["args"] = args
+
+        post_save.connect(receiver)
+        call_command("test_command", "--disable-model-signals")
+        assert received == {
+            "sender": ManagementCommandLog,
+            "args": (),
+        }
 
 
 @pytest.mark.django_db
