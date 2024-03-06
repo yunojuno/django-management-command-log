64e1c048e5f3e6c0015a77ac61a63b75cc7d6174
Hugo Rodger-Brown <hugo@yunojuno.com>
2024-03-06 15:27:33 +0000

    Add admin list filter for command status

--- command_log/admin.py
+++ command_log/admin.py
@@ -4,6 +4,8 @@ import ast
 import json
 
 from django.contrib import admin
+from django.db import models
+from django.http import HttpRequest
 from django.utils.safestring import mark_safe
 
 from command_log.models import ManagementCommandLog
@@ -18,9 +20,41 @@ def pretty_print(data: dict | None) -> str:
     return mark_safe("<pre><code>%s</code></pre>" % html)  # noqa: S308, S703
 
 
+class StatusListFilter(admin.SimpleListFilter):
+    title = "status"
+    parameter_name = "status"
+
+    def lookups(
+        self,
+        request: HttpRequest,
+        model_admin: ManagementCommandLogAdmin,
+    ) -> tuple[tuple[str, str], ...]:
+        return (
+            ("0", "In progress"),
+            ("1", "Complete"),
+        )
+
+    def queryset(
+        self,
+        request: HttpRequest,
+        queryset: models.QuerySet[ManagementCommandLog],
+    ) -> models.QuerySet[ManagementCommandLog]:
+        if self.value() == "0":
+            return queryset.filter(started_at__isnull=False, finished_at__isnull=True)
+        if self.value() == "1":
+            return queryset.filter(started_at__isnull=False, finished_at__isnull=False)
+        return queryset
+
+
 class ManagementCommandLogAdmin(admin.ModelAdmin):
     list_display = ("management_command", "started_at", "duration", "exit_code_display")
-    list_filter = ("started_at", "app_name", "command_name", "exit_code")
+    list_filter = (
+        "started_at",
+        "app_name",
+        "command_name",
+        "exit_code",
+        StatusListFilter,
+    )
     search_fields = ("command_name",)
     readonly_fields = (
         "management_command",
